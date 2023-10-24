d66e4856f2e4ce559b40c5158bdb672833a0601c
deveshparashar <93967981+deveshparashar@users.noreply.github.com>
2023-10-24 15:12:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,11 +20,11 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  type = "map"
+  type = map
   default     {
-       "name" =  "ec2-instance-jenkins-pipeline"
-      "owner" = "703343570"
-      "OHR" = "703343570"
+       name =  "ec2-instance-jenkins-pipeline"
+      owner = "7033435322322370"
+      OHR = "7033435232323232270"
 }
 
 variable "ami_id" {
