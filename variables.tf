5a6e78a90250082080af47ff085911191273406d
deveshparashar <93967981+deveshparashar@users.noreply.github.com>
2023-10-24 15:02:18 +0530

    Update variables.tf added tags

--- variables.tf
+++ variables.tf
@@ -21,6 +21,8 @@ variable "security_group" {
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
   default     = "ec2-instance-jenkins-pipeline"
+  owner = "703343570"
+  OHR = "703343570"
 }
 
 variable "ami_id" {
